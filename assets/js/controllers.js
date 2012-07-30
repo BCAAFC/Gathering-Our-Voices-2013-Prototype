afca93a0cadc09d227aa4c1dce8b4c42047a7ff7
Andrew Hobden <andy.hobden@gmail.com>
2012-07-30 18:07:48 +0000

    Some general cleaning.

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,12 +1,22 @@
 'use strict';
 
 /* Controllers */
-function regCtl($scope, $http) {
+function regCtl($scope, $http, $anchorScroll) {
 	$scope.submit = function (){
-		$http.post('/register', { Name: $scope.Name });
-	}
+		$http.post('/register', 
+				{ 
+					primaryName 		: $scope.primaryName,
+					primaryAddress 	: $scope.primaryAddress,
+					primaryLocation : $scope.primaryLocation,
+					primaryPhone		:	$scope.primaryPhone,
+					primaryEmail		:	$scope.primaryEmail
+				});
+	};
+	$scope.scrollto = function goToByScroll(id){
+		$anchorScroll(id);
+	};
 };
-regCtl.$inject = ['$scope','$http'];
+regCtl.$inject = ['$scope','$http', '$anchorScroll'];
 
 
 function homeCtl() {};
