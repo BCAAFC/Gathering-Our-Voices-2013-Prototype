4850f0e6a889f6c33fda7d79549b995f89ddddd6
Andrew Hobden <andy.hobden@gmail.com>
2012-08-08 18:45:53 +0000

    Got chaperones working properly via filter

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('gov', ['ui']).
+angular.module('gov', ['ui','govmath']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.when('/register', {templateUrl: 'partials/register', controller: regCtl});
     $routeProvider.when('/management', {templateUrl: 'partials/management', controller: manCtl});
@@ -11,3 +11,10 @@ angular.module('gov', ['ui']).
 		$routeProvider.otherwise({redirectTo: '/'});
     $locationProvider.html5Mode(true);
   }]);
+
+angular.module('govmath', []).
+	filter('chaperone', function() {
+		return function(num) {
+			return Math.ceil(num / 5);
+		};
+	});
